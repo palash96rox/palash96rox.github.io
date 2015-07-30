574bfffd0104d7bfd4304d0cab900a2051996221
Palash Agarwal <palash.agarwal.96@gmail.com>
2015-07-31 03:31:51 +0530

    First actual commit

---
+++ static/index.js
@@ -0,0 +1,5 @@
+console.log("Welcome to the website!");
+console.log("Check me out on GitHub: https://github.com/palash96rox");
+console.log("Check me out on Facebook: https://www.facebook.com/palash.96");
+console.log("Check me out on LinkedIn: https://www.linkedin.com/in/palash27a");
+console.log("Check me out on Twitter: https://twitter.com/palash96_a");
