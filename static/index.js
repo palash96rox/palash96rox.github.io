4e9c492cbe84d0300ac15869ce3ebcf74345aeec
Palash Agarwal <palash.agarwal.96@gmail.com>
2015-08-02 18:49:36 +0530

    Tooltips

--- static/index.js
+++ static/index.js
@@ -4,47 +4,23 @@
  * Logs welcome message onto the console
  */
 function logWelcome() {
-	console.log("Welcome to the website!");
-	console.log("Check me out on GitHub: https://github.com/palash96rox");
-	console.log("Check me out on Facebook: https://www.facebook.com/palash.96");
-	console.log("Check me out on LinkedIn: https://www.linkedin.com/in/palash27a");
-	console.log("Check me out on Twitter: https://twitter.com/palash96_a");
+	console.log("Welcome to my website!");
+	console.log("Thanks a lot to Font-Awesome: http://fortawesome.github.io/Font-Awesome/")
 }
 
 /**
  * Sets the links for various social profiles
  */
 function setLinks() {
-	document.getElementById("fb").onclick = function () {
-		var n = window.open("https://www.facebook.com/palash.96", "_blank");
-		n.focus();
-	};
-	document.getElementById("whatsapp").onclick = function () {
-		window.open("tel:xxx-xxx-xxxx", "");
-	};
-	document.getElementById("insta").onclick = function () {
-		var n = window.open("https://instagram.com/palash_a/", "_blank");
-		n.focus();
-	};
-	document.getElementById("skype").onclick = function () {
-		window.open("SkypeID: palash.agarwal27", "_blank");
-	};
-	document.getElementById("linkedin").onclick = function () {
-		var n = window.open("https://www.linkedin.com/in/palash27a", "_blank");
-		n.focus();
-	};
-	document.getElementById("twitter").onclick = function () {
-		var n = window.open("https://twitter.com/palash96_a", "_blank");
-		n.focus();
-	};
-	document.getElementById("gmail").onclick = function () {
-		var n = window.open("http://mail.google.com/mail/?view=cm&fs=1&tf=1&to=palash.agarwal.96@gmail.com", "_blank");
-		n.focus();
-	};
-	document.getElementById("github").onclick = function () {
-		var n = window.open("https://github.com/palash96rox", "_blank");
-		n.focus();
-	};
+	document.getElementById("fb").onclick = function () {window.open("https://www.facebook.com/palash.96");};
+	document.getElementById("cv").onclick = function () {window.open("./static/Resume.pdf");};
+	document.getElementById("insta").onclick = function () {window.open("https://instagram.com/palash_a/");};
+	document.getElementById("skype").onclick = function () {window.open("SkypeID: palash.agarwal27");};
+	document.getElementById("linkedin").onclick = function () {window.open("https://www.linkedin.com/in/palash27a");};
+	document.getElementById("twitter").onclick = function () {window.open("https://twitter.com/palash96_a");};
+	document.getElementById("gmail").onclick = function () {window.open("http://mail.google.com/mail/?view=cm&fs=1&tf=1&to=palash.agarwal.96@gmail.com");};
+	document.getElementById("github").onclick = function () {window.open("https://github.com/palash96rox");};
+	//Blog: document.getElementById("wait").onclick = function () {window.open("");};
 }
 
 /**
